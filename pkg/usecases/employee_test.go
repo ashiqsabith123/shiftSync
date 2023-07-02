92296595d29f1c13e5787b46fd9b8ad0ff3d0ba2
Ashiq Sabith <ashiqsabith328@gmail.com>
2023-07-02 13:32:44 +0530

    Signup fucntion tested in usecase
    -Tested with two test cases
    -all tests are succesfully passed
    -with coverage of around 2

--- pkg/usecases/employee_test.go
+++ pkg/usecases/employee_test.go
@@ -1,25 +1,68 @@
 package usecases
 
 import (
+	"context"
+	"errors"
 	"shiftsync/pkg/domain"
 	mock "shiftsync/pkg/mock/employeeRepoMock"
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSignup(t *testing.T) {
-
 	cntrl := gomock.NewController(t)
+	defer cntrl.Finish()
 
 	mockRepo := mock.NewMockEmployeeRepository(cntrl)
 
 	employeeUsecase := NewEmployeeUseCase(mockRepo)
 
 	testData := []struct {
-		name        string
+		responce    string
 		employee    domain.Employee
 		expectedErr error
-	}{}
+		beforeTest  func(employeeRepo *mock.MockEmployeeRepository)
+	}{
+		{
+			responce: "Test success",
+			employee: domain.Employee{
+				First_name: "Ashiq",
+				Last_name:  "Sabith",
+				Phone:      8606863748,
+				Email:      "ashiqsabith328@gmail.com",
+				User_name:  "ashiqsabith328",
+				Pass_word:  "Ashiq@123",
+			},
+			beforeTest: func(employeeRepo *mock.MockEmployeeRepository) {
+				employeeRepo.EXPECT().AddEmployee(gomock.Any(), gomock.Any()).Return(nil)
+			},
+			expectedErr: nil,
+		},
+		{
+			responce: "Test failure",
+			employee: domain.Employee{
+				First_name: "Ashiq",
+				Last_name:  "Sabith",
+				Phone:      8606863748,
+				Email:      "ashiqsabith328@gmail.com",
+				User_name:  "ashiqsabith328",
+				Pass_word:  "Ashiq@123",
+			},
+			beforeTest: func(employeeRepo *mock.MockEmployeeRepository) {
+				employeeRepo.EXPECT().AddEmployee(gomock.Any(), gomock.Any()).Return(errors.New("Employee already exists"))
+			},
+			expectedErr: errors.New("Employee already exists"),
+		},
+	}
 
+	for _, testCase := range testData {
+		t.Run(testCase.responce, func(t *testing.T) {
+			testCase.beforeTest(mockRepo)
+			err := employeeUsecase.SignUp(context.Background(), testCase.employee)
+			assert.Equal(t, testCase.expectedErr, err)
+
+		})
+	}
 }
