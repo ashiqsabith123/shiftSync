bc1e3cc849b2f8ddd3dffa8308770567d01ef741
Ashiq Sabith <ashiqsabith328@gmail.com>
2023-09-12 20:35:31 +0530

    Made some changes in cron.go
    -Changed some functions names in that
    -To improve coding standard

--- cronautomate/cron.go
+++ cronautomate/cron.go
@@ -27,7 +27,7 @@ func AutomateCreditSalary() {
 
 func CreditSalary() {
 
-	IDs, idErr := GetAllIds()
+	IDs, idErr := getAllIds()
 
 	if idErr != nil || len(IDs) == 0 {
 		return
@@ -38,14 +38,14 @@ func CreditSalary() {
 
 		var totalAmount float32
 
-		hours, calulateError := CalculateTotalWorkingHours(v, currentMonth)
+		hours, calulateError := calculateTotalWorkingHours(v, currentMonth)
 		if calulateError != nil {
 			continue
 		}
 
 		duties := hours / 8
 
-		grade, gradeError := GetGradeOfTheEmployee(v)
+		grade, gradeError := getGradeOfTheEmployee(v)
 		if gradeError != nil {
 			continue
 		}
@@ -59,7 +59,7 @@ func CreditSalary() {
 			totalAmount = hours * 100.0
 		}
 
-		allowance, allowanceError := AddAllAlowances(v)
+		allowance, allowanceError := addAllAlowances(v)
 		if allowanceError != nil {
 			continue
 		}
@@ -68,22 +68,19 @@ func CreditSalary() {
 
 		getCount.Id = v
 		getCount.Date = int(time.Now().Month())
-		fmt.Println(getCount)
 
-		paidLeave, leaveError := GetCountOfPaidLeave(getCount)
+		paidLeave, leaveError := getCountOfPaidLeave(getCount)
 		if leaveError != nil {
 			fmt.Println(leaveError)
 		}
 
 		leaveCount := paidLeave
 
-		fmt.Println("paid leave", paidLeave)
-
 		if paidLeave != 0 {
 			paidLeave = paidLeave * 100
 		}
 
-		deductions, deductionError := CaculateDeductions(v)
+		deductions, deductionError := caculateDeductions(v)
 		if deductionError != nil {
 			continue
 		}
@@ -91,7 +88,7 @@ func CreditSalary() {
 		grossSalary := totalAmount + allowance + float32(paidLeave)
 		netSalary := grossSalary - deductions
 
-		if err := UpdateFullSalary(v, paidLeave, leaveCount, int(duties), grossSalary, netSalary); err != nil {
+		if err := updateFullSalary(v, paidLeave, leaveCount, int(duties), grossSalary, netSalary); err != nil {
 			continue
 		}
 
@@ -99,7 +96,7 @@ func CreditSalary() {
 			continue
 		}
 
-		accDetails := FetchAccDetailsById(v)
+		accDetails := fetchAccDetailsById(v)
 
 		accDetails.ID = v
 
@@ -118,7 +115,7 @@ func CreditSalary() {
 
 }
 
-func CalculateTotalWorkingHours(id int, month string) (float32, error) {
+func calculateTotalWorkingHours(id int, month string) (float32, error) {
 	var hours float32
 	if err := db.GetDatabaseInstance().Raw("SELECT CAST(SUM(EXTRACT(epoch FROM (TO_TIMESTAMP(punch_out, 'HH24:MI:SS') - TO_TIMESTAMP(punch_in, 'HH24:MI:SS')))) AS FLOAT) / 3600 AS hours FROM attendances WHERE employee_id = ? AND date_trunc('month', TO_DATE(date, 'YYYY-MM-DD')) = date_trunc('month', TO_DATE(?, 'YYYY-MM-DD'));", id, month).Scan(&hours).Error; err != nil {
 		return 0, err
@@ -127,7 +124,7 @@ func CalculateTotalWorkingHours(id int, month string) (float32, error) {
 	return hours, nil
 }
 
-func GetGradeOfTheEmployee(id int) (string, error) {
+func getGradeOfTheEmployee(id int) (string, error) {
 	var grade string
 	if err := db.GetDatabaseInstance().Raw("SELECT grade FROM salaries 	WHERE employee_id = ?", id).Scan(&grade).Error; err != nil {
 		return "", err
@@ -136,7 +133,7 @@ func GetGradeOfTheEmployee(id int) (string, error) {
 	return grade, nil
 }
 
-func AddAllAlowances(id int) (float32, error) {
+func addAllAlowances(id int) (float32, error) {
 	var allowance float32
 	if err := db.GetDatabaseInstance().Raw("SELECT (d_allowance + sp_allowance + m_allowance) AS allowance FROM salaries WHERE employee_id = ?", id).Scan(&allowance).Error; err != nil {
 		return 0, err
@@ -145,7 +142,7 @@ func AddAllAlowances(id int) (float32, error) {
 	return allowance, nil
 }
 
-func CaculateDeductions(id int) (float32, error) {
+func caculateDeductions(id int) (float32, error) {
 	var deductions float32
 	if err := db.GetDatabaseInstance().Raw("SELECT (tax + provident_fund ) AS deductions FROM salaries WHERE employee_id = ?", id).Scan(&deductions).Error; err != nil {
 		return 0, err
@@ -154,14 +151,14 @@ func CaculateDeductions(id int) (float32, error) {
 	return deductions, nil
 }
 
-func UpdateFullSalary(id, leave, leave_count, duties int, gross, net float32) error {
+func updateFullSalary(id, leave, leave_count, duties int, gross, net float32) error {
 	if err := db.GetDatabaseInstance().Exec("UPDATE salaries SET gross_salary = ? ,net_salary = ? , leave_pay = ? , duties = ?, leave_count = ? WHERE employee_id = ?", gross, net, leave, duties, leave_count, id).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
-func FetchAccDetailsById(id int) response.AccDetails {
+func fetchAccDetailsById(id int) response.AccDetails {
 	var details response.AccDetails
 	if err := db.GetDatabaseInstance().Raw("SELECT forms.account_no, razorpays.contact_id FROM forms INNER JOIN razorpays ON forms.form_id = razorpays.id WHERE forms.form_id = ?;", id).Scan(&details).Error; err != nil {
 		log.Fatal(err)
@@ -170,7 +167,7 @@ func FetchAccDetailsById(id int) response.AccDetails {
 	return details
 }
 
-func GetCountOfPaidLeave(reqCount response.LeaveCount) (int, error) {
+func getCountOfPaidLeave(reqCount response.LeaveCount) (int, error) {
 	var count int
 
 	if err := db.GetDatabaseInstance().Raw("SELECT SUM(DATE_PART('day', leaves.to::timestamp - leaves.from::timestamp)) AS count FROM leaves WHERE employee_id = ? AND mode = 'P' AND status = 'A' AND EXTRACT(MONTH FROM to_date(leaves.from, 'DD-MM-YYYY')) =?;", reqCount.Id, reqCount.Date).Scan(&count).Error; err != nil {
@@ -180,7 +177,7 @@ func GetCountOfPaidLeave(reqCount response.LeaveCount) (int, error) {
 	return count, nil
 }
 
-func GetAllIds() ([]int, error) {
+func getAllIds() ([]int, error) {
 	var id []int
 	if err := db.GetDatabaseInstance().Raw("SELECT form_id FROM FORMS WHERE status = 'A'").Scan(&id).Error; err != nil {
 		return id, err
